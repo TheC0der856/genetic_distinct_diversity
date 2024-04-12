3800611cf7105fae7a06381a1c3806b32971bfb6
SarahGronefeld <146098953+SarahGronefeld@users.noreply.github.com>
2024-04-12 16:42:45 +0200

    Add files via upload
    median insteaed of NA, KBA applied

--- genetic_D&D__KBA_criteria.R
+++ genetic_D&D__KBA_criteria.R
@@ -32,120 +32,157 @@ datasets <- c("Ambystoma_bishopi",
 all_thresholds_met <- data.frame()
 
 for (dataset in datasets) {
-  
-  amova <- read.csv(file= paste("Results_", dataset, "/AMOVA/amova_results.csv", sep = ""))
-  # create a table like in the descriptions of the KBA standard
-  # calculate percentages of variance between sites 
-  sum_Difference <- sum(amova$Difference)
-  amova$percentage <- NA
-  for (i in 1:length(amova$Difference)) {
-    amova$percentage[i] <- (amova$Difference[i] / sum_Difference) * 100
-  }
-  # add a row to the table if the threshold was met
-  amova$threshold_met <- ifelse(amova$percentage > 10, "B1, A1b", ifelse(amova$percentage > 1, "A1b", "no protection"))
-  
-  
-  lamda <- read.csv(file= paste("Results_", dataset, "/diversity/diversity_results.csv", sep = ""))
-  # create a table like in the descriptions of the KBA standard
-  lamda <- lamda %>%
-    slice(1:(n() - 1))
-  # calculate percentages of variance between sites 
-  sum <- sum(lamda$X.N..N...1.....lambda)
-  lamda$percentage <- NA
-  for (i in 1:length(lamda$X.N..N...1.....lambda)) {
-    lamda$percentage[i] <- (lamda$X.N..N...1.....lambda[i] / sum) * 100
-  }
-  # add a row to the table if the threshold was met
-  lamda$threshold_met <- ifelse(lamda$percentage > 10, "B1, A1b", ifelse(lamda$percentage > 1, "A1b", "no protection"))
-  
-  
-  Eco <- read.csv(file= paste("Results_",dataset,"/Overlaps/Overlaps_results.csv", sep=""))
-  # create a table like in the descriptions of the KBA standard
-  # add a row to the table if the threshold was met
-  Eco$threshold_met <- ifelse(Eco$observed_overlap_percentages > 10, "B1, A1b", ifelse(Eco$observed_overlap_percentages > 1, "A1b", "no protection"))
-  
-  AvTD <- read.csv(file= paste("Results_", dataset, "/AvTD/AvTD_results.csv", sep = ""))
-  # create a table like in the descriptions of the KBA standard
-  AvTD <- AvTD %>%
-    slice(1:(n() - 1))
-  # calculate percentages of variance between sites 
-  sum <- sum(AvTD$Delta..1)
-  AvTD$percentage <- NA
-  for (i in 1:length(AvTD$Delta..1)) {
-    AvTD$percentage[i] <- (AvTD$Delta..1[i] / sum) * 100
-  }
-  # add a row to the table if the threshold was met
-  AvTD$threshold_met <- ifelse(AvTD$percentage > 10, "B1, A1b", ifelse(AvTD$percentage > 1, "A1b", "no protection"))
-  
-  
-  # Combine threshold met information!
-  thresholds_met <- data.frame(
-    sites = amova$site,
-    amova = amova$threshold_met,
-    Eco = Eco$threshold_met,
-    lamda = lamda$threshold_met, 
-    AvTD = AvTD$threshold_met,
-    NeS = NA, 
-    NeE = NA, 
-    dataset = dataset
-  )
-  
-  
+  # data sets can be only successfully compared if there is data from all methods and programs
+  # There should not be a data set which consists of NA only, because otherwise an average to replace missing values with cannot be calculated
   if (file.exists(paste("Results_", dataset, "/NeEstimator/Ne_results.csv", sep = ""))) {
-    NeE <- read.csv(file= paste("Results_", dataset, "/NeEstimator/Ne_results.csv", sep = ""))
+    if (file.exists(paste("Results_",dataset ,"/NeSpeed/Ne_results.csv", sep = ""))) { 
+        
+      amova <- read.csv(file= paste("Results_", dataset, "/AMOVA/amova_results.csv", sep = ""))
+      # create a table like in the descriptions of the KBA standard
+      # calculate percentages of variance between sites 
+      sum_Difference <- sum(amova$Difference)
+      amova$percentage <- NA
+      for (i in 1:length(amova$Difference)) {
+        amova$percentage[i] <- (amova$Difference[i] / sum_Difference) * 100
+      }
+      # add a row to the table if the threshold was met
+      amova$threshold_met <- ifelse(amova$percentage > 10, "B1, A1b", ifelse(amova$percentage > 1, "A1b", "no protection"))
+      
+      
+      lamda <- read.csv(file= paste("Results_", dataset, "/diversity/diversity_results.csv", sep = ""))
+      # create a table like in the descriptions of the KBA standard
+      lamda <- lamda %>%
+        slice(1:(n() - 1))
+      # calculate percentages of variance between sites 
+      sum <- sum(lamda$X.N..N...1.....lambda)
+      lamda$percentage <- NA
+      for (i in 1:length(lamda$X.N..N...1.....lambda)) {
+        lamda$percentage[i] <- (lamda$X.N..N...1.....lambda[i] / sum) * 100
+      }
+      # add a row to the table if the threshold was met
+      lamda$threshold_met <- ifelse(lamda$percentage > 10, "B1, A1b", ifelse(lamda$percentage > 1, "A1b", "no protection"))
+      
+      
+      Eco <- read.csv(file= paste("Results_",dataset,"/Overlaps/Overlaps_results.csv", sep=""))
+      # create a table like in the descriptions of the KBA standard
+      # add a row to the table if the threshold was met
+      Eco$threshold_met <- ifelse(Eco$observed_overlap_percentages > 10, "B1, A1b", ifelse(Eco$observed_overlap_percentages > 1, "A1b", "no protection"))
+      
+      AvTD <- read.csv(file= paste("Results_", dataset, "/AvTD/AvTD_results.csv", sep = ""))
+      # create a table like in the descriptions of the KBA standard
+      AvTD <- AvTD %>%
+        slice(1:(n() - 1))
+      # calculate percentages of variance between sites 
+      sum <- sum(AvTD$Delta..1)
+      AvTD$percentage <- NA
+      for (i in 1:length(AvTD$Delta..1)) {
+        AvTD$percentage[i] <- (AvTD$Delta..1[i] / sum) * 100
+      }
+      # add a row to the table if the threshold was met
+      AvTD$threshold_met <- ifelse(AvTD$percentage > 10, "B1, A1b", ifelse(AvTD$percentage > 1, "A1b", "no protection"))
+      
     
-    # # create a table like in the descriptions of the KBA standard
-    # # calculate percentages of variance between sites 
-    # sum <- sum(NeE$Ne)
-    # NeE$percentage <- NA
-    # for (i in 1:length(NeE$Ne)) {
-    #   NeE$percentage[i] <- (NeE$Ne[i] / sum) * 100
-    # }
-    # # add a row to the table if the threshold was met
-    # NeE$threshold_met <- ifelse(NeE$percentage > 10, "B1, A1b", ifelse(NeE$percentage > 1, "A1b", ""))
-    # 
-    # # add NeE information
-    # thresholds_met$NeE[amova$site %in% NeE$site] <- NeE$threshold_met[match(amova$site[amova$site %in% NeE$site], NeE$site)]
-    
-    # Since in most datasets except of Syncerus caffer and Pagophila eburnea there are missing sites, 
-    # calculating the percentage would create biased results: 
-    # the remaining sites would be worth a higher protection status as if all sites would have been calculated.
-    # Therefore the proposed thresholds of Garnier et al. 2020, Hoban et al. 2021 and Leike et al. 2020 of
-    # Ne < 50 (VI = very important) and Ne < 500 (I = important) should be used. 
-    
-    NeE$threshold_met <- ifelse(NeE$Ne < 50, "Ne < 50",
-                                ifelse(NeE$Ne < 500, "Ne < 500" , "no protection"))
-    # add the thresholds to the dataframe:
-    thresholds_met$NeE[amova$site %in% NeE$site] <- NeE$threshold_met[match(amova$site[amova$site %in% NeE$site], NeE$site)]
-  }
-  
-  if(file.exists(paste("Results_",dataset ,"/NeSpeed/Ne_results.csv", sep = ""))) {
-    NeS <- read.csv(file= paste("Results_",dataset ,"/NeSpeed/Ne_results.csv", sep = ""))
-    
-    # # create a table like in the descriptions of the KBA standard
-    # # calculate percentages of variance between sites 
-    # sum <- sum(NeS$Ne)
-    # NeS$percentage <- NA
-    # for (i in 1:length(NeS$Ne)) {
-    #   NeS$percentage[i] <- (NeS$Ne[i] / sum) * 100
-    # }
-    # # add a row to the table if the threshold was met
-    # NeS$threshold_met <- ifelse(NeS$percentage > 10, "B1, A1b", ifelse(NeS$percentage > 1, "A1b", ""))
-    # 
-    # Since in most datasets except of Syncerus caffer and Pagophila eburnea there are missing sites, 
-    # calculating the percentage would create biased results: 
-    # the remaining sites would be worth a higher protection status as if all sites would have been calculated.
-    # Therefore the proposed thresholds of Garnier et al. 2020, Hoban et al. 2021 and Leike et al. 2020 of
-    # Ne < 50 (VI = very important) and Ne < 500 (I = important) should be used. 
-    NeS$threshold_met <- ifelse(NeS$Ne < 50,  "Ne < 50", 
-                                ifelse(NeS$Ne < 500, "Ne < 500", "no protection"))
-    
-    # add NeS information
-    thresholds_met$NeS[amova$site %in% NeS$site] <- NeS$threshold_met[match(amova$site[amova$site %in% NeS$site], NeS$site)]
-  }
-
-  all_thresholds_met <- rbind(all_thresholds_met, thresholds_met)
-
+      NeE <- read.csv(file= paste("Results_", dataset, "/NeEstimator/Ne_results.csv", sep = ""))
+      #check if there are missing information that can be added
+      if (length(setdiff(amova$site, NeE$site)) != 0) {  
+        # add missing data to the table
+        missing_sites <- setdiff(amova$site, NeE$site) # find missing sites
+        missing_data <- data.frame(site = missing_sites, 
+                                    Ne = NA,
+                                    lower_jackknife_confidence_interval = NA,
+                                    upper_jackknife_confidence_interval = NA,
+                                    lower_parametric_confidence_interval = NA,
+                                    upper_parametric_confidence_interval = NA)
+        NeE <- rbind(NeE, missing_data)
+      }
+      # create a table like in the descriptions of the KBA standard
+      # test wheater there are missing values
+      if (anyNA(NeE$Ne)) { # if there are NA
+        # calculate the median without NA
+        median_Ne <- median(NeE$Ne, na.rm = TRUE)
+        # replace missing values with the mean
+        NeE$Ne <- ifelse(is.na(NeE$Ne), median_Ne, NeE$Ne)
+        # calculate percentages of variance between sites 
+        sum <- sum(NeE$Ne)
+        NeE$percentage <- NA
+        for (i in 1:length(NeE$Ne)) {
+          NeE$percentage[i] <- (NeE$Ne[i] / sum) * 100
+        }
+      } else { # There are no NA
+        # calculate percentages of variance between sites 
+        sum <- sum(NeE$Ne)
+        NeE$percentage <- NA
+        for (i in 1:length(NeE$Ne)) {
+          NeE$percentage[i] <- (NeE$Ne[i] / sum) * 100
+        }
+      }
+        
+      # add a row to the table if the threshold was met
+      NeE$threshold_met <- ifelse(NeE$percentage > 10, "B1, A1b",
+                                  ifelse(NeE$percentage > 1, "A1b", "no protection"))
+      
+      
+      
+      NeS <- read.csv(file= paste("Results_",dataset ,"/NeSpeed/Ne_results.csv", sep = ""))
+      
+      #check if there are missing information that can be added
+      if (length(setdiff(amova$site, NeS$site)) != 0)  {  # NeS has not the same sites like amova!
+        # add missing data to the table
+        missing_sites <- setdiff(amova$site, NeS$site) # find missing sites
+        missing_data <- data.frame(site = missing_sites, 
+                                   Ne = NA,
+                                   lower_97_5_interval_percentile = NA,
+                                   upper_97_5_interval_percentile = NA,
+                                   lower_97_5_interval_parametric = NA,
+                                   upper_97_5_interval_parametric = NA)
+        NeS <- rbind(NeS, missing_data)
+      }
+      if (length(setdiff(NeS$site, amova$site)) != 0) {
+        # delete unnecessary sites
+        sites_to_remove <- setdiff(NeS$site, amova$site)
+        NeS <- subset(NeS, !site %in% sites_to_remove)
+      }
+      # create a table like in the descriptions of the KBA standard
+      # test wheater there are missing values
+      if (anyNA(NeS$Ne)) { # if there are NA
+        # calculate the median without NA
+        median_NeS <- median(NeS$Ne, na.rm = TRUE)
+        # replace missing values with the mean
+        NeS$Ne <- ifelse(is.na(NeS$Ne), median_NeS, NeE$Ne)
+        # calculate percentages of variance between sites 
+        sum <- sum(NeS$Ne)
+        NeS$percentage <- NA
+        for (i in 1:length(NeS$Ne)) {
+          NeS$percentage[i] <- (NeS$Ne[i] / sum) * 100
+        }
+      } else { # There are no NA
+        # calculate percentages of variance between sites 
+        sum <- sum(NeS$Ne)
+        NeSpercentage <- NA
+        for (i in 1:length(NeS$Ne)) {
+          NeS$percentage[i] <- (NeS$Ne[i] / sum) * 100
+        }
+      }
+      
+      # add a row to the table if the threshold was met
+      NeS$threshold_met <- ifelse(NeS$percentage > 10, "B1, A1b",
+                                  ifelse(NeS$percentage > 1, "A1b", "no protection"))
+      
+      # Combine threshold met information!
+      thresholds_met <- data.frame(
+        sites = amova$site,
+        amova = amova$threshold_met,
+        Eco = Eco$threshold_met,
+        lamda = lamda$threshold_met, 
+        AvTD = AvTD$threshold_met,
+        NeS = NeS$threshold_met, 
+        NeE = NeE$threshold_met, 
+        dataset = dataset
+      )
+      
+    } else {next} 
+  } else {next}
+all_thresholds_met <- rbind(all_thresholds_met, thresholds_met)
 }
 
 # # save as xlsx -file
@@ -192,7 +229,7 @@ pie(table(all_thresholds_met$lamda),
 #legend("topright", legend = c("A1b > 1%" , "B1 > 10%"))
 
 pie(table(all_thresholds_met$AvTD),
-    main = "\u0394+", 
+    main = expression(bold(Delta["j"]^"+")), 
     labels = rep("", length(table(all_thresholds_met$NeE))),
     col=c("cornsilk1", "lightskyblue1", "salmon3"))
 
@@ -206,7 +243,7 @@ pie(table(all_thresholds_met$NeE, useNA = "ifany"),
     #           "missing information"),
     main = "Ne Estimator", 
     labels = rep("", length(table(all_thresholds_met$NeE))),
-    col=c( "royalblue4", "lightgoldenrod", "salmon3", "white"))
+    col=c( "cornsilk1", "lightskyblue1", "salmon3", "white"))
 
 
 pie(table(all_thresholds_met$NeS, useNA = "ifany"),
@@ -216,7 +253,7 @@ pie(table(all_thresholds_met$NeS, useNA = "ifany"),
     #           "missing information"),
     main = "Speed Ne",
     labels = rep("", length(table(all_thresholds_met$NeE))),
-    col=c("royalblue4", "lightgoldenrod", "salmon3", "white"))
+    col=c("cornsilk1", "lightskyblue1", "salmon3", "white"))
 
 plot.new()
 plot.new()
@@ -224,10 +261,10 @@ par(xpd = TRUE)
 legend("center",
        legend = c("> 1% (A1b)" ,
                   "> 10% (B1, A1b)",
-                  "no protection",
-                  expression(italic(N)[italic("e")] < 500),
-                  expression(italic(N)[italic("e")] < 50),
-                  "missing information"), 
+                  "no protection"),
+                  #expression(italic(N)[italic("e")] < 500),
+                  #expression(italic(N)[italic("e")] < 50),
+                  #"missing information"), 
         fill = c("cornsilk1", "lightskyblue1", "salmon3", "lightgoldenrod","royalblue4", "white"), 
         cex = 1.3)
 
