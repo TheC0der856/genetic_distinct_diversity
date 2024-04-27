2a2a39c4cb07ce815138915211fe5a63096d32e1
SarahGronefeld <146098953+SarahGronefeld@users.noreply.github.com>
2024-04-27 14:23:26 +0200

    Add files via upload
    Now calculating mean instead of substituting missing val with zero

--- genetic_D&D__AvTD.R
+++ genetic_D&D__AvTD.R
@@ -9,8 +9,8 @@ if (!file.exists(output_folder_path)) {
 }
 
 
-######################### Calculate allele frequencies #####################################
-
+######################### Format data frame into an Genind Object #####################################
+######################### to enable easy processing with R ############################################
 
 # Save the ID of the individual as a vector to add it later to the genind object
 individual <- as.vector(original_file[1]$...1)
@@ -27,15 +27,15 @@ if (ploidy == 2 ) {
   # Information is lost about a known allele next to a missing allele at a locus.  
   # Negative numbers (e.g. -9) and 0 are searched for and transformed into NAs.
   
-  # convert 0 into -9:
+  # convert 0 into NA:
   # it is easier to search for a - instead of 0s later on and replace them like this
   for (locus in names(data_frame_without_information)) { # all column names
     for (i in 1:nrow(data_frame_without_information)) { # index every row
       # if the data point is not NA, but is 0 it can be converted (asking only for 0s if there are NAs in the data set, can cause trouble)
       if (!is.na(data_frame_without_information[i, locus]) && data_frame_without_information[i, locus] == 0) {
-        data_frame_without_information[i, locus] <- 0  # convert 0!
+        data_frame_without_information[i, locus] <- NA  # convert 0!
       } else if (is.na(data_frame_without_information[i, locus])) {
-        data_frame_without_information[i, locus] <- 0  # convert <NA>! why? because <NA> needs to be the entry for both alleles and not only one.
+        data_frame_without_information[i, locus] <- NA  # convert <NA>! why? because <NA> needs to be the entry for both alleles and not only one.
       }
     }
   }
@@ -47,23 +47,22 @@ if (ploidy == 2 ) {
         unite(!!colnames(data_frame_without_information)[columns_number], c(!!colnames(data_frame_without_information)[columns_number], !!colnames(data_frame_without_information)[columns_number+1]), sep = " ")
     }
   }
-  # convert negative numbers into 0:
+  # convert negative numbers into NA:
   for (locus in names(data_frame_formatted)) {
     missing_values <- grepl("-", data_frame_formatted[, locus])
     if (any(missing_values)) {
-      data_frame_formatted[missing_values, locus] <- 0
+      data_frame_formatted[missing_values, locus] <- NA
     }
   }
-  # convert "NA" into 0
+  # convert "NA" into NA
   for (locus in names(data_frame_formatted)) {
     missing_values <- grepl("NA", data_frame_formatted[, locus])
     if (any(missing_values)) {
-      data_frame_formatted[missing_values, locus] <- 0
+      data_frame_formatted[missing_values, locus] <- NA
     }
   }
   # How many missing values does my data set have?
   # sum(is.na(data_frame_formatted))
-  
   
 } else if (ploidy == 1) {
   data_frame_formatted <- data_frame_without_information
@@ -71,17 +70,17 @@ if (ploidy == 2 ) {
   for (locus in names(data_frame_formatted)) {
     missing_values <- grepl("-", data_frame_formatted[, locus])
     if (any(missing_values)) {
-      data_frame_formatted[missing_values, locus] <- 0
+      data_frame_formatted[missing_values, locus] <- NA
     }
   }
-  # # convert 0:
-  # for (locus in names(data_frame_formatted)) {
-  #   #  Check if " " occurs in the column
-  #   if (any(data_frame_formatted[, locus] == 0)) {
-  #     # replace "0" with "NA" in every column
-  #     data_frame_formatted[data_frame_formatted[, locus] == 0, locus] <- 0
-  #   }
-  # }
+  # convert 0:
+  for (locus in names(data_frame_formatted)) {
+     #  Check if " " occurs in the column
+     if (any(data_frame_formatted[, locus] == 0)) {
+       # replace "0" with "NA" in every column
+       data_frame_formatted[data_frame_formatted[, locus] == 0, locus] <- NA
+     }
+   }
 } else {
   cat("This ploidy cannot be calculated.")
 }
@@ -92,6 +91,9 @@ colnames(data_frame_formatted) <- gsub("\\.", "_", colnames(data_frame_formatted
 
 
 Genind <- df2genind(data_frame_formatted, pop= population, ind.names = individual , sep = " ", ploidy = ploidy)
+
+######################### Remove bad quality data ##################################################
+
 # check if the user determined a threshold for missing values that should be removed
 if (exists('threshold_percentage_of_missing_values_that_is_removed')) { # if the threshold exists
   Genind <- missingno(Genind, type = method_type_to_handle_missing_values, cutoff = threshold_percentage_of_missing_values_that_is_removed, quiet = FALSE, freq = FALSE)
@@ -99,85 +101,70 @@ if (exists('threshold_percentage_of_missing_values_that_is_removed')) { # if the
   Genind <- missingno(Genind, type = method_type_to_handle_missing_values, quiet = FALSE, freq = FALSE)
 }  
 
+################################### calculate distances ############################
 
-########################### Calculate allele frequencies ######################################
-# set a counter
-locus_index <- 0
-# and an empty dataframe
-allele_frequencies <- data.frame(locus_allel_name = character(), stringsAsFactors = FALSE)
+# Calculate allele abundances per population 
+# default setting will calculate the mean of NA
+allele_abundances <- genind2genpop(Genind)
 
-for (locus in names(Genind@all.names)) {
-  # With each loop pass over the loci, the locus_index is increased by 1 (counts at which locus is currently being calculated).
-  locus_index <- locus_index + 1
-  # allele per locus:
-  locus_allel_name <- colnames(Genind@tab)[grepl(names(Genind@all.names)[locus_index], colnames(Genind@tab))]
+
+if (!exists("distance_method")) {
   
-  # Create an empty data frame to store results:
-  # The first column in the data frame contains the names of the alleles.
-  # All further columns should later contain the allele frequencies for each population of the investigated locus.
-  # The population names are already entered as column labels of the still empty columns.
+  # switch columns and rows
+  allele_abundances_tab <- t(allele_abundances@tab)
+  # Taxonomic distances from a classification table with variable step lengths
+  taxdis <- taxa2dist(allele_abundances_tab, varstep=TRUE)
+  # Calculate AvTD
+  mod <- taxondive(allele_abundances@tab, taxdis)
+} else {
+  # Calculate allele frequencies
+  allele_frequencies <- makefreq(allele_abundances,missing="mean",quiet=TRUE)
+  X <- t(allele_frequencies)
   
-  
-  # Small information on the subject of missing values:
-  # If 0s and negative numbers are not converted into NAs the following command can be used to omit missing values (-9):
-  # locus_allel_name <- locus_allel_name[!grepl("-9", locus_allel_name)]
-  # the calculation will produce similar results like GenAlEx if you use these commands:
-  # sum_allele_frequencies_for_locus_for_pop <- colSums(allele_frequency[, -1])
-  # allele_frequency <- sweep(allele_frequency[, -1], 2, sum_allele_frequencies_for_locus_for_pop, " ")
-  # after "Add the frequencies of the allele in the population to the data frame:"
-  # Eventhough information is lost about a known allele next to a missing allele at a locus, 
-  # I decided to use missingno() to handle missing data instead of the "locus_allel_name <- locus_allel_name[!grepl("-9", locus_allel_name)]"-calculation, 
-  # because missingno() provides many options for the user and is more difficult to avoid in the AMOVA calculation (all methods should be as comparable as possible)
-  # If you want to change this Code to loose fewer information, I consider "locus_allel_name <- locus_allel_name[!grepl("-9", locus_allel_name)]" as a valid option. 
-  # Other calculation methods to handle missing values could be also inserted here e.g. the "mean" method of missingno()...
-  
-  
-  allele_frequency <- data.frame(locus_allel_name)
-  for (population in unique(Genind@pop)) {
-    name_of_column <- paste(population)
-    allele_frequency[[name_of_column]] <- 0
+  # Berechnung aus ?dist.genpop()
+  if (distance_method == "Edward") {
+    # Edwards distance
+    # Also have a look at: ?edwards.dist() of poppr package
+    # nloc can be translated into the number of columns/ populations/ areas
+    nloc <- ncol(X)
+    X <- sqrt(X)
+    d <- X%*%t(X)
+    d <- 1-d/nloc
+    diag(d) <- 0
+    d <- sqrt(d)
+    d <- as.dist(d)
+  } else if (distance_method == "Nei"){
+    # Neis distance
+    d <- X%*%t(X)
+    vec <- sqrt(diag(d))
+    d <- d/vec[col(d)]
+    d <- d/vec[row(d)]
+    d <- -log(d)
+    d <- as.dist(d)
+  } else if (distance_method == "pairwise"){
+    # raw pairwise distances
+    # https://rdrr.io/r/stats/dist.html , 
+    # https://grunwaldlab.github.io/poppr/reference/poppr.amova.html 
+    d <- dist(X, method = "euclidean", diag = FALSE, upper = FALSE, p = 2)
+  } else {
+    cat("The method you specified to calculate the distance between loci is not existing.")
   }
   
-  # For all populations, the frequency of an allele in the population is to be calculated and stored:
-  for (population in unique(Genind@pop)) {
-    # calculate the frequency for each allele in the population:
-    allele_frequencies_for_locus_for_population <- sapply(locus_allel_name, function(locus_allel_name) {
-      allele_per_individual_in_population <- Genind@tab[Genind@pop == population, locus_allel_name]
-      sum(allele_per_individual_in_population)
-    })
-    # Add the frequencies of the allele in the population to the data frame:
-    allele_frequency[, as.character(population)] <- allele_frequencies_for_locus_for_population
-    # Rename data frame after locus name
-    assign(locus, allele_frequency)
-  }
-}
-# All created tables are merged into one large table
-for (locus in names(Genind@all.names)) {
-  # Call up data frame
-  little_allele_table <- get(locus)
-  # Add data frame to large table
-  allele_frequencies <- rbind(allele_frequencies, little_allele_table)
-}
-# Reset the row labels in the large table
-rownames(allele_frequencies) <- NULL
-
-
-# In order to use the allele frequencies for further calculations, the data must be cleaned from error sources introduced by missingno().
-# If the "mean" option is selected, rounding of the values is necessary.
-allele_frequencies <- round(allele_frequencies[-1])
-# Lines that add up to 0 must be deleted.
-row_sum_is_zero <- allele_frequencies[rowSums(allele_frequencies[-1] != 0) == 0, ]
-allele_frequencies <- allele_frequencies[!(rownames(allele_frequencies) %in% rownames(row_sum_is_zero)), ]
-
-################# Calculate AvTD ################
-# switch columns and rows
-t_allele <- t(allele_frequencies)
-# Taxonomic distances from a classification table with variable step lengths
-taxdis <- taxa2dist(allele_frequencies, varstep=TRUE)
-# Calculate AvTD
-mod <- taxondive(t_allele, taxdis)
-
+  
+  ################# Calculate AvTD ################
+  # pure d can produce NaN, that's why d will be scaled
+  # the highest value should be 100 likewise to taxondive
+  # preserve the ratio between all distances within the matrix
+  max <- max(d)
+  multiplier <- 100 /max
+  scaled_d <- d * multiplier
+  # calculate AvTD
+  # plot(hclust(scaled_d), hang = -1)
+  mod <- taxondive(allele_abundances@tab, scaled_d)
+  #summary(mod)
+  
+}  
+  
 ############### Save results ####################
 write.csv(summary(mod), file = paste(output_folder_path, "/AvTD_results.csv", sep = ""), row.names = TRUE)
-
 
